6bcfea0beebcf2267aaf7a70ec53656752ebff99
matthiasmeissen <meissenmatthias@gmail.com>
2026-02-06 17:43:20 +0100

    Add Timer Interrupt

--- src/main.rs
+++ src/main.rs
@@ -2,9 +2,13 @@
 #![no_std]
 #![no_main]
 
-use cortex_m::{self, asm};
+use core::cell::RefCell;
+use cortex_m::{self, asm, interrupt::Mutex};
 use embedded_hal::digital::OutputPin;
 use rp235x_hal as hal;
+use hal::timer::Alarm;
+use hal::pac::interrupt;
+use hal::fugit::MicrosDurationU32;
 use panic_halt as _;
 use defmt_rtt as _;
 use defmt;
@@ -13,9 +17,50 @@ use defmt;
 #[used]
 pub static IMAGE_DEF: hal::block::ImageDef = hal::block::ImageDef::secure_exe();
 
+type AlarmType = rp235x_hal::timer::Alarm0<rp235x_hal::timer::CopyableTimer0>;
+
+static SHARED_STATE: Mutex<RefCell<Option<AlarmType>>> = Mutex::new(RefCell::new(None));
+
+#[interrupt]
+fn TIMER0_IRQ_0() {
+    cortex_m::interrupt::free(|cs| {
+        if let Some(alarm0) = SHARED_STATE.borrow(cs).borrow_mut().as_mut() {
+            alarm0.clear_interrupt();
+            defmt::info!("Counter");
+
+            alarm0.schedule(MicrosDurationU32::millis(500)).unwrap();
+        }
+    })
+}
+
 #[hal::entry]
 fn main() -> ! {
     let mut pac = hal::pac::Peripherals::take().unwrap();
+
+    // Initialize Clocks
+    let mut watchdog = hal::Watchdog::new(pac.WATCHDOG);
+    let clocks = hal::clocks::init_clocks_and_plls(
+        12_000_000u32, 
+        pac.XOSC, 
+        pac.CLOCKS, 
+        pac.PLL_SYS, 
+        pac.PLL_USB, 
+        &mut pac.RESETS, 
+        &mut watchdog,
+    ).unwrap();
+
+    let mut timer = hal::Timer::new_timer0(
+        pac.TIMER0, 
+        &mut pac.RESETS, 
+        &clocks,
+    );
+    let mut alarm0: AlarmType = timer.alarm_0().unwrap();
+    let _ = alarm0.schedule(MicrosDurationU32::millis(500)).unwrap();
+    alarm0.enable_interrupt();
+
+    cortex_m::interrupt::free(|cs| {
+        SHARED_STATE.borrow(cs).replace(Some(alarm0));
+    });
 
     let sio = hal::Sio::new(pac.SIO);
 
@@ -23,13 +68,16 @@ fn main() -> ! {
         pac.IO_BANK0, 
         pac.PADS_BANK0, 
         sio.gpio_bank0, 
-        &mut pac.RESETS
+        &mut pac.RESETS,
     );
 
     defmt::info!("Test");
 
     let mut led_pin = pins.gpio25.into_push_pull_output();
     led_pin.set_high().unwrap();
+
+    unsafe { cortex_m::peripheral::NVIC::unmask(hal::pac::Interrupt::TIMER0_IRQ_0) };
+    cortex_m::peripheral::NVIC::unpend(hal::pac::Interrupt::TIMER0_IRQ_0);
 
     loop {
         asm::wfi();
