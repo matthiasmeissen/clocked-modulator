d13356fe57b0bcdff2ada3d28d045ebfd8134473
matthiasmeissen <meissenmatthias@gmail.com>
2026-02-07 16:05:15 +0100

    Add custom formatting to phasor

--- src/phasor.rs
+++ src/phasor.rs
@@ -1,4 +1,5 @@
-#[derive(Debug, defmt::Format)]
+
+#[derive(Debug)]
 pub struct PhasorBank {
     phasor_d2: f32,
     phasor_x1: f32,
@@ -27,5 +28,30 @@ impl PhasorBank {
         self.phasor_d2 = (self.phasor_d2 + self.base_increment * 0.5) % 1.0;
         self.phasor_x1 = (self.phasor_x1 + self.base_increment) % 1.0;
         self.phasor_x2 = (self.phasor_x2 + self.base_increment * 2.0) % 1.0;
+    }
+}
+
+// Custom Formatting of Phasor Values
+const BAR_WIDTH: usize = 32;
+
+fn format_bar(value: f32) -> [u8; BAR_WIDTH] {
+    let filled = (value * BAR_WIDTH as f32) as usize;
+    let mut bar = [b'_'; BAR_WIDTH];
+    for b in bar.iter_mut().take(filled.min(BAR_WIDTH)) {
+        *b = b'#';
+    }
+    bar
+}
+
+impl defmt::Format for PhasorBank {
+    fn format(&self, f: defmt::Formatter) {
+        let d2 = format_bar(self.phasor_d2);
+        let x1 = format_bar(self.phasor_x1);
+        let x2 = format_bar(self.phasor_x2);
+        defmt::write!(f, "d2: {} | x1: {} | x2: {}",
+            core::str::from_utf8(&d2).unwrap(),
+            core::str::from_utf8(&x1).unwrap(),
+            core::str::from_utf8(&x2).unwrap(),
+        );
     }
 }
